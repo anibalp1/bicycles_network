b9483728723fd7dbddd428030a5138b9b3bda10a
Samuel Peña <samuelpena098@gmail.com>
2020-11-11 22:16:18 -0400

    added update and delete test api methods

--- spec/api/bicycle_api_test_spec.js
+++ spec/api/bicycle_api_test_spec.js
@@ -2,6 +2,10 @@ var Bicycle = require('../../models/bicycle');
 var request = require('request');
 var server = require('../../bin/www');
 
+beforeEach(()=>{
+    Bicycle.allBicycles = [];
+});
+
 describe('Bicycle Api', ()=> {
     describe('Get Bicycles /', ()=> {
         it('Status 200', ()=> {
@@ -28,6 +32,47 @@ describe('Bicycle Api', ()=> {
             }, function(error, response, body){
                 expect(response.statusCode).toBe(200);
                 expect(Bicycle.findByid(10).color).toBe("red");
+                done();
+            });
+        });
+    });
+
+    describe('Post Bicycle /update', ()=> {
+        it('Status 200', (done)=> {
+            var headers = {'content-type' : 'application/json'};
+            var bic = new Bicycle(10, "red", "urban", [18.477770, -69.915592]);
+            Bicycle.add(bic);
+            
+            var updatedBic = '{"id":10, "color":"blue", "model":"mountain", "location": [18.477770, -69.915592]}';
+
+            request.post({
+                headers: headers,
+                url: 'http://localhost:3000/api/bicycles/update',
+                body: updatedBic
+            }, function(error, response, body){
+                expect(response.statusCode).toBe(200);
+                expect(Bicycle.findByid(10).color).toBe("blue");
+                expect(Bicycle.findByid(10).model).toBe("mountain");
+
+                done();
+            });
+        });
+    });
+
+    describe('Post Bicycle /delete', ()=> {
+        it('Status 200', (done)=> {
+            var headers = {'content-type' : 'application/json'};
+            var bic = new Bicycle(10, "red", "urban", [18.477770, -69.915592]);
+            Bicycle.add(bic);
+
+            request.delete({
+                headers: headers,
+                url: 'http://localhost:3000/api/bicycles/delete',
+                body: '{"id": 10}'
+            }, function(error, response, body){
+                expect(response.statusCode).toBe(204);
+                expect(Bicycle.allBicycles.length).toBe(0);
+
                 done();
             });
         });
