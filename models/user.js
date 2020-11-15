cf6f65f81a9128c9ba5eae1f769866bf5edda2f9
Samuel Peña <samuelpena098@gmail.com>
2020-11-14 22:50:11 -0400

    Added more info into the user schema including password encryption

--- models/user.js
+++ models/user.js
@@ -2,9 +2,50 @@ var mongoose = require('mongoose');
 var Reservation = require('./reservation');
 var Schema = mongoose.Schema;
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const validateEmail = function(email){
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return re.test(email);
+}
+
 var userSchema = new Schema({
-    name: String,
-})
+    name: {
+        type: String, 
+        trim: true,
+        required: [true, 'The name is required']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'The email is required'],
+        lowercase: true, 
+        validate: [validateEmail, 'write a valid email'],
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'The password is required'],
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    validated: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+userSchema.methods.validatePassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 userSchema.methods.reserve = function (bicId, startDate, endDate, callback){
     var reserve = new Reservation({
