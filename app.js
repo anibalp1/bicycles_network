4dc2ef67951ac2c9280d3708c62d9ff9d803d1eb
Samuel Peña <samuelpena098@gmail.com>
2020-11-23 00:32:08 -0400

    added sendgrid to the project and added missing method to the dotenv declaration inside app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var jwt = require('jsonwebtoken');
-var dotenv = require('dotenv');
+var dotenv = require('dotenv').config();
 const passport = require('./config/passport');
 const session = require('express-session');
 
--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,35 @@
 const nodeMailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'sammy.white@ethereal.email',
-        pass: 'd3MJG6ptYS25wa4Ug6'
+let mailConfig;
+if(process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+
+} else{ 
+    if(process.env.NODE_ENV === 'staging'){
+        console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+
+    }else{
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ethereal_user, 
+                pass: process.env.ethereal_pass
+            }
+        }; 
+    }
+}
 
 module.exports = nodeMailer.createTransport(mailConfig); 
